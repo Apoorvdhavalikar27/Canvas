a7ce91c326a32bd81ede4472b3907d31b46378ce
Aniruddha <41411202+Apoorvdhavalikar27@users.noreply.github.com>
2024-04-17 11:24:32 +0530

    Add files via upload
    File upload

---
+++ ChatHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNetCore.Cors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SingleRChat
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            string connectionId = Context.ConnectionId;
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+            //Clients.All.broadcastMessage(connectionId, name, message);
+           
+        }
+
+    }
+}
